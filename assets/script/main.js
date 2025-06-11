8a40a3c3415ba16c3a225bbcb7eb795cc4d4d798
biz-htacchi <div.htacchi67@gmail.com>
2025-06-11 15:35:21 +0900

    accodion : 完了

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,5 +1,5 @@
 //==============================================//
-// Swip
+// Swiper
 //==============================================//
 var swiper = new Swiper('.swiper', {
   loop: true,
@@ -25,4 +25,25 @@ var swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+//==============================================//
+// Accordion
+//==============================================//
+$(document).ready(function() {
+    // 初期状態では全ての回答を非表示にする
+    $(".q-and-a__accordion-qa .q-and-a__answer").hide();
+
+    // 質問（dt）がクリックされたときの処理
+    $(".q-and-a__accordion-qa .q-and-a__question").on("click", function() {
+        // クリックされた質問の次の要素（回答: dd）をスライドで表示・非表示を切り替える
+        $(this).next(".q-and-a__answer").slideToggle("fast");
+
+        // クリックされた質問に 'open' クラスをトグルで追加・削除する
+        $(this).toggleClass("open");
+
+        // 他の開いている質問があれば閉じる
+        $(".q-and-a__accordion-qa .q-and-a__question").not(this).removeClass("open");
+        $(".q-and-a__accordion-qa .q-and-a__answer").not($(this).next(".q-and-a__answer")).slideUp("fast");
+    });
 });
